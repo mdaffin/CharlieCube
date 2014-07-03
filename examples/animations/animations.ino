f9e2123f1cce73524fd567b9d3092f903ed99345
Michael Daffin <james1479@gmail.com>
2014-07-04 00:37:44 +0100

    Makes the animation more intresting

--- examples/animations/animations.ino
+++ examples/animations/animations.ino
@@ -3,28 +3,26 @@
 void setup() {
   Cube::begin();
   fountian(255, 0, 0);
+  fountian(0, 0, 0);
   fountian(0, 255, 0);
+  fountian(0, 0, 0);
   fountian(0, 0, 255);
-}
-
-void spinner(uint8_t red, uint8_t green, uint8_t blue) {
-  int speed = 50;
-
+  fountian(0, 0, 0);
 }
 
 void fountian(uint8_t red, uint8_t green, uint8_t blue) {
   int speed = 100;
 
   for (int i = 0; i < 4; i++) {
-    Cube::clear();
-    Cube::drawBox(0,0,i, 3,3,i, red, green, blue);
-    Cube::drawBox(1,1,i, 2,2,i, 0, 0, 0);
+    Cube::drawBox(1,1,i, 2,2,i, red, green, blue);
     Cube::flush();
     delay(speed);
   }
   for (int i = 3; i >= 0; i--) {
-    Cube::clear();
-    Cube::drawBox(1,1,i, 2,2,i, red, green, blue);
+    Cube::drawBox(0,0,i, 3,0,i, red, green, blue);
+    Cube::drawBox(0,0,i, 0,3,i, red, green, blue);
+    Cube::drawBox(3,3,i, 3,0,i, red, green, blue);
+    Cube::drawBox(3,3,i, 0,3,i, red, green, blue);
     Cube::flush();
     delay(speed);
   }
@@ -40,5 +38,6 @@ void loop() {
     blue = random(2);
   }
   fountian(red, green, blue);
+  fountian(0, 0, 0);
 }
 
